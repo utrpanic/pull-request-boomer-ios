154b215294b26627d89c6d3c221b509217f3593a
box-jeon <utrpanic@gmail.com>
2021-07-05 23:15:46 +0900

    SwiftPackage. Import ModernRIBs

--- BoomerLib/Package.swift
+++ BoomerLib/Package.swift
@@ -13,8 +13,7 @@ let package = Package(
             targets: ["BoomerLib", "Model", "Service"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        
+        .package(url: "https://github.com/DevYeom/ModernRIBs.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
