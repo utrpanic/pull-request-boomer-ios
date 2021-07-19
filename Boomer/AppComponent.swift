434a9c04dd8275f95660d4e5a068812406d6f65c
box-jeon <utrpanic@gmail.com>
2021-07-19 12:44:04 +0900

    Remove old project.

--- Boomer/AppComponent.swift
+++ Boomer/AppComponent.swift
@@ -2,13 +2,7 @@ import InterfaceLib
 import ModelLib
 import ModernRIBs
 
-final class AppDependency: EmptyDependency, HasDependencyProvider {
+final class AppComponent: MainDependency, HasDependencyProvider {
     var apis: ApiProviderProtocol = ApiProvider()
     var buildables: BuildableProviderProtocol = BuildableProvider()
-}
-
-final class AppComponent: Component<EmptyDependency>, MainDependency {
-    init() {
-        super.init(dependency: AppDependency())
-    }
 }
--- PullRequestBoomer/Notifications/LocalEvents.swift
+++
@@ -1,5 +0,0 @@
-import BoxKit
-
-struct AuthSucceed: LocalEvent { }
-
-struct AuthFailure: LocalEvent { }
